a27196f81c70d9f37dd35ad91d7b8a0863f197b8
Javier Santos <javsanmar5@alum.us.es>
2025-01-20 19:24:57 +0100

    Save each service in its own Bucket

--- storage/storage.go
+++ storage/storage.go
@@ -14,7 +14,6 @@ import (
 
 func AddPassword(service, username, password string) {
 	dbPath := getDBPath()
-	fmt.Println(dbPath)
 
 	db, err := bolt.Open(dbPath, 0600, nil)
 	if err != nil {
@@ -23,14 +22,13 @@ func AddPassword(service, username, password string) {
 	defer db.Close()
 
 	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte("Passwords"))
+		bucket, err := tx.CreateBucketIfNotExists([]byte(service))
 		if err != nil {
 			return fmt.Errorf("failed to create bucket: %v", err)
 		}
 
-		data := fmt.Sprintf("username: %s, password: %s", username, password)
-
-		return bucket.Put([]byte(service), []byte(data))
+		// TODO: Encrypt the password before storing it
+		return bucket.Put([]byte(username), []byte(password))
 	})
 	if err != nil {
 		log.Fatalf("Failed to update database: %v", err)
