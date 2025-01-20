0248238f21b36cb90825ff06307454bf92bb1805
Javier Santos <javsanmar5@alum.us.es>
2025-01-20 17:37:36 +0100

    Remove unnecessary import

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
 import "github.com/javsanmar5/passwordmanager/cmd"
-import "github.com/spf13/cobra"
 
 func main() {
 	cmd.Execute()
