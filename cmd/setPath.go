8bbafc4db53e54fca8a701af9020c51f78decdb9
Javier Santos <javsanmar5@alum.us.es>
2025-01-23 13:17:30 +0100

    cli: Add set-path command Refs #1

---
+++ cmd/setPath.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2025 Javier Santos javier.jsm21@gmail.com
+*/
+package cmd
+
+import (
+	"github.com/javsanmar5/stormy/utils"
+	"github.com/spf13/cobra"
+)
+
+var setPathCmd = &cobra.Command{
+	Use:   "set-path",
+	Short: "Set the path for the database",
+	Long: `Set the path for the database.
+This command allows you to specify a new path and optionally force 
+the change without confirmation.
+
+This action will move the database to the new path if exists.
+
+Example:
+	# To set the path for the database to a new location
+	stormy set-path
+	# It will ask you for the new path and confirm the change`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		utils.SetPath(force)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(setPathCmd)
+
+	setPathCmd.Flags().BoolVarP(&force, "force", "f", false, "Force the path change without confirmation")
+}
--- utils/path.go
+++ utils/path.go
@@ -4,15 +4,61 @@ Copyright © 2025 Javier Santos javier.jsm21@gmail.com
 package utils
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 func getDBPath() string {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("Failed to get home directory: %v", err)
+	var err error
+
+	_ = godotenv.Load()
+	path := os.Getenv("DB_PATH")
+
+	if path == "" {
+		path, err = os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("Failed to get home directory: %v", err)
+		}
 	}
-	return filepath.Join(homeDir, ".stormy.db")
+
+	return filepath.Join(path, ".stormy.db")
+}
+
+func SetPath(force bool) {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Which path do you want to use for the database?(./):")
+	path, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read the input: %v", err)
+	}
+	newEnv := "DB_PATH=" + strings.TrimSpace(path)
+
+	if !force {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Are you sure you want to change the database path? [y/N]: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("Failed to read the input: %v", err)
+		}
+
+		input = strings.ToLower(strings.TrimSpace(input))
+		if input == "y" || input == "yes" {
+			force = true
+		}
+	}
+
+	if force {
+		d1 := []byte(newEnv)
+		err = os.WriteFile(".env", d1, 0644)
+		if err != nil {
+			log.Fatalf("Failed to write to .env file: %v", err)
+		}
+	}
 }
